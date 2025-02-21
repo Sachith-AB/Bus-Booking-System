2c67dfea8776c585a0b7d29ff69b4f34eb138091
Sachith Avintha <153298821+Sachith45@users.noreply.github.com>
2025-02-21 15:03:26 +0530

    create login and register initial UI

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,10 @@
-import 'package:bus_booking/firebase_options.dart';
-import 'package:firebase_core/firebase_core.dart';
+import 'package:bus_booking/views/auth/login/login.dart';
+import 'package:bus_booking/views/auth/register/register.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
-void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
+
+void main() {
   runApp(const MyApp());
 }
 
@@ -22,34 +19,11 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const HomePage(),
-    );
-  }
-}
-
-class HomeController extends GetxController {
-  var message = "Welcome to Bus Booking App!".obs;
-}
-
-class HomePage extends StatelessWidget {
-  const HomePage({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    final HomeController controller = Get.put(HomeController());
-
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text("Home Page"),
-        backgroundColor: Colors.deepPurple,
-        foregroundColor: Colors.white,
-      ),
-      body: Center(
-        child: Obx(() => Text(
-              controller.message.value,
-              style: const TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-            )),
-      ),
+      initialRoute: '/register',  // Start with Login Page
+      getPages: [
+        GetPage(name: '/login', page: () => const LoginPage()),
+        GetPage(name: '/register', page: () => const RegisterPage()),
+      ],
     );
   }
 }
